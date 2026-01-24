f86ac7078569cf6d653737fbc897610ea6a54e6d
Robotorgovec <bakalim.ser@gmail.com>
2026-01-25 03:48:41 +0500

    Change HTML language attribute from 'en' to 'ru'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
-    <html lang="en">
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import type { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="ru">
       <body>{children}</body>
     </html>
   );
