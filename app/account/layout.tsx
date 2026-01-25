695f61fcb458b778eaa5473e8d4c096020642b31
Robotorgovec <bakalim.ser@gmail.com>
2026-01-25 05:13:18 +0500

    Style account auth forms

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -9,6 +9,7 @@ export default function AccountLayout({ children }: { children: ReactNode }) {
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,200,0,0"
       />
       <link rel="stylesheet" href="/assets/layout.css" />
+      <link rel="stylesheet" href="/assets/css/auth.css" />
       <a className="skip" href="#main">
         К содержанию
       </a>
