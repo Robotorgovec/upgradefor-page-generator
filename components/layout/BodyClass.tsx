7fa387377decc23e75013143d7adb4cd2347253c
Robotorgovec <bakalim.ser@gmail.com>
2026-02-08 02:59:46 +0500

    Use global event to toggle TopNotice

--- components/layout/BodyClass.tsx
+++ components/layout/BodyClass.tsx
@@ -5,7 +5,12 @@ import { useEffect } from "react";
 export default function BodyClass({ className }: { className: string }) {
   useEffect(() => {
     document.body.classList.add(className);
-    return () => document.body.classList.remove(className);
+    const main = document.getElementById("main");
+    main?.classList.add(className);
+    return () => {
+      document.body.classList.remove(className);
+      main?.classList.remove(className);
+    };
   }, [className]);
 
   return null;
