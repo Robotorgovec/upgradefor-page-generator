d829a5ccf0ad3ede89ab1b156cc1bf0b10e3824a
Robotorgovec <bakalim.ser@gmail.com>
2026-03-03 10:53:07 +0500

    Add Cu-Al menu icon

---
+++ app/wikimarket/hvac/copper-aluminum-heat-exchangers/page.tsx
@@ -0,0 +1,11 @@
+import CuAlHeatExchangersPage from "../../../../components/wikimarket/hvac/copper-aluminum-heat-exchangers/CuAlHeatExchangersPage";
+
+export const metadata = {
+  title: "Медно-алюминиевые теплообменники (Cu-Al) — проектирование, производство и поставка",
+  description:
+    "Калориферы, воздухоохладители, испарители и конденсаторы Cu-Al для вентиляции, ПУ, чиллеров, руфтопов и кондиционеров. Подбор под режимы, чертежи, документы, КП.",
+};
+
+export default function CopperAluminumHeatExchangersRoute() {
+  return <CuAlHeatExchangersPage />;
+}
