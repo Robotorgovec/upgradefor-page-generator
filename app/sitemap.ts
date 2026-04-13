50552a236bf9cb5f7dcc57a51e9c0bcec5d698d1
Robotorgovec <bakalim.ser@gmail.com>
2026-04-13 01:43:32 -0700

    Fix sitemap import to relative path for build resolution

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from 'next';
 
-import { getLearningGameSitemapPaths } from '@/lib/learning-game/sitemap';
+import { getLearningGameSitemapPaths } from '../lib/learning-game/sitemap';
 
 const BASE_URL = 'https://upgradefor.com';
 
